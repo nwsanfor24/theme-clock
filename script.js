09dfd8038512fca20261e609ce6a3ccbcb3b424c
Nathan Sanford <nwsanfor24@gmail.com>
2021-02-13 12:54:49 -0500

    js query selectors

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
+const hourEl = document.querySelector('.hour')
+const minuteEl = document.querySelector('.minute')
+const secondEl = document.querySelector('.second')
+const timeEl = document.querySelector('.time')
+const dateEl = document.querySelector('.date')
+const toggle = document.querySelector('.toggle')
+
+const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
 
